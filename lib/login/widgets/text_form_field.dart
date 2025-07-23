13a488dd259a7eef0a132209e88481089e0ed755
FatmaIsmail101 <ismailfatma197@gmail.com>
2025-07-23 22:08:37 +0300

    Login Screen

---
+++ lib/login/widgets/text_form_field.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextFormField extends StatelessWidget {
+  const TextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextFormField(
+
+        )
+      ],
+    );
+
+  }
+}
